06b701fe1db3fa043d16f56f38c277ad0f08ce32
jacobrein <jacob.rein@gabb.com>
2024-05-22 14:12:39 -0600

    - cleaning up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,5 +69,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("com.google.accompanist:accompanist-drawablepainter:0.35.1-alpha")
+    implementation(libs.accompanist.drawablepainter)
 }
